8d2c0f5c0c5570cc7f515bfb6330770f492bbcd5
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-04-26 22:41:01 -0500

    Agregado register.html

--- src/main/java/com/WAEC2GRUPO4/ExamenT2/configuration/AppConfig.java
+++ src/main/java/com/WAEC2GRUPO4/ExamenT2/configuration/AppConfig.java
@@ -1,4 +1,5 @@
 package com.WAEC2GRUPO4.ExamenT2.configuration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -6,7 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 public class AppConfig {
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-}
+    }
 }
--- src/main/java/com/WAEC2GRUPO4/ExamenT2/controller/SeguridadController.java
+++ src/main/java/com/WAEC2GRUPO4/ExamenT2/controller/SeguridadController.java
@@ -2,7 +2,12 @@ package com.WAEC2GRUPO4.ExamenT2.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SeguridadController {
+    @GetMapping("register")
+    public String register(){
+        return "register";
+    }
 }
